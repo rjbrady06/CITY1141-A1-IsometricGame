4dafd88453476e19ee48ac4fd66f144bba5f5ecb
rjbrady06 <1383622@cityplym.ac.uk>
2025-03-07 15:47:56 +0000

    fixing the rotation
    changed some rotation code to try and fix some problems with the snapping of the rotation angles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,16 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rb;
     [SerializeField] private float _speed = 3;
+    [SerializeField] private float _turnSpeed = 360;
+
+
     private Vector3 _input;
 
 
     void Update()
     {
         GatherInput();
+        Look();
     } 
 
     void FixedUpdate()
@@ -24,8 +28,24 @@ public class PlayerController : MonoBehaviour
         _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
    }
 
+    void Look()
+    {
+        if (_input != Vector3.zero)
+        {
+            var matrix = Matrix4x4.Rotate(Quaternion.Euler(0,45,0));
+
+            var skewedInput = matrix.MultiplyPoint3x4(_input);  
+
+
+            var relative = (transform.position + _input) - transform.position;
+            var rot = Quaternion.LookRotation(relative, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation,rot, _turnSpeed * Time.deltaTime);
+        }
+    }
+
     void Move()
     {
-        _rb.MovePosition(transform.position + transform.forward * _speed * Time.deltaTime);
+        _rb.MovePosition(transform.position + (transform.forward * _input.magnitude)* _speed * Time.deltaTime);
     }
 }
